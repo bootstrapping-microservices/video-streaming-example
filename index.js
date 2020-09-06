6c6a2dc3f113f31152e6e7e1e0e64958aefd4da3
Ashley Davis <ashley@codecapers.com.au>
2020-09-07 08:16:36 +1000

    Video streaming example that works in Chrome and Safari.

---
+++ index.js
@@ -0,0 +1,118 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.static("public"));
+
+const filePath = "./videos/SampleVideo_1280x720_1mb.mp4";
+
+app.get("/works-in-chrome", (req, res) => {
+    res.setHeader("content-type", "video/mp4");
+    
+    fs.stat(filePath, (err, stat) => {
+        if (err) {
+            console.error(`File stat error for ${filePath}.`);
+            console.error(err);
+            res.sendStatus(500);
+            return;
+        }
+
+        res.setHeader("content-length", stat.size);
+
+        const fileStream = fs.createReadStream(filePath);
+        fileStream.on("error", error => {
+            console.log(`Error reading file ${filePath}.`);
+            console.log(error);
+            res.sendStatus(500);
+        });
+
+        fileStream.pipe(res)
+    });
+});
+
+app.get('/works-in-chrome-and-safari', (req, res) => {
+
+    const options = {};
+
+    let start;
+    let end;
+
+    const range = req.headers.range;
+    if (range) {
+        const bytesPrefix = "bytes=";
+        if (range.startsWith(bytesPrefix)) {
+            const bytesRange = range.substring(bytesPrefix.length);
+            const parts = bytesRange.split("-");
+            if (parts.length === 2) {
+                const rangeStart = parts[0] && parts[0].trim();
+                if (rangeStart && rangeStart.length > 0) {
+                    options.start = start = parseInt(rangeStart);
+                }
+                const rangeEnd = parts[1] && parts[1].trim();
+                if (rangeEnd && rangeEnd.length > 0) {
+                    options.end = end = parseInt(rangeEnd);
+                }
+            }
+        }
+    }
+
+    res.setHeader("content-type", "video/mp4");
+
+    fs.stat(filePath, (err, stat) => {
+        if (err) {
+            console.error(`File stat error for ${filePath}.`);
+            console.error(err);
+            res.sendStatus(500);
+            return;
+        }
+
+        let contentLength = stat.size;
+
+        if (req.method === "HEAD") {
+            res.statusCode = 200;
+            res.setHeader("accept-ranges", "bytes");
+            res.setHeader("content-length", contentLength);
+            res.end();
+        }
+        else {        
+            let retreivedLength;
+            if (start !== undefined && end !== undefined) {
+                retreivedLength = (end+1) - start;
+            }
+            else if (start !== undefined) {
+                retreivedLength = contentLength - start;
+            }
+            else if (end !== undefined) {
+                retreivedLength = (end+1);
+            }
+            else {
+                retreivedLength = contentLength;
+            }
+
+            res.statusCode = start !== undefined || end !== undefined ? 206 : 200;
+
+            res.setHeader("content-length", retreivedLength);
+
+            if (range !== undefined) {  
+                res.setHeader("content-range", `bytes ${start || 0}-${end || (contentLength-1)}/${contentLength}`);
+                res.setHeader("accept-ranges", "bytes");
+            }
+
+            const fileStream = fs.createReadStream(filePath, options);
+            fileStream.on("error", error => {
+                console.log(`Error reading file ${filePath}.`);
+                console.log(error);
+                res.sendStatus(500);
+            });
+            
+            fileStream.pipe(res);
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
